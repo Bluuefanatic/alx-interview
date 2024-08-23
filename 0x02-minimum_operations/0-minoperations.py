425400f68971813b08d0c28fc723bf345bc51dfb
root <izirenjoel@gmail.com>
2024-08-23 23:20:53 +0000

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,9 +3,10 @@
 Minimum Operations Module
 """
 
+
 def minOperations(n):
     """
-    Calculates the minimum number of operations needed to result in exactly n 'H' characters
+    Calculates the minimum number of operations needed
     Args:
         n (int): The target number of 'H' characters
     Returns:
@@ -13,14 +14,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
