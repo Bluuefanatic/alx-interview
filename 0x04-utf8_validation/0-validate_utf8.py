1a2e068147d39f4053bd38dac1147ca9b8a6377a
root <izirenjoel@gmail.com>
2024-09-05 02:48:44 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,42 +1,29 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
 def validUTF8(data):
-    # Helper function to check if a byte is a continuation byte
-    def is_continuation(byte):
-        return (byte & 0xC0) == 0x80
+    """
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
+    """
+    byte_count = 0
 
-    # Number of bytes remaining in the current UTF-8 sequence
-    num_bytes = 0
-
-    for byte in data:
-        # Determine the number of bytes in the UTF-8 sequence
-        if num_bytes == 0:
-            # Check the leading byte
-            if (byte & 0x80) == 0x00:
-                # 1-byte sequence (0xxxxxxx)
-                num_bytes = 0
-            elif (byte & 0xE0) == 0xC0:
-                # 2-byte sequence (110xxxxx 10xxxxxx)
-                num_bytes = 1
-            elif (byte & 0xF0) == 0xE0:
-                # 3-byte sequence (1110xxxx 10xxxxxx 10xxxxxx)
-                num_bytes = 2
-            elif (byte & 0xF8) == 0xF0:
-                # 4-byte sequence (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
-                num_bytes = 3
-            else:
-                # Invalid byte
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            # Check continuation bytes
-            if not is_continuation(byte):
+            if i >> 6 != 0b10:
                 return False
-            num_bytes -= 1
-
-    # Check if all sequences were properly ended
-    return num_bytes == 0
-
-# Example usage
-data = [197, 130, 1]  # Valid UTF-8 encoding
-print(validUTF8(data))  # Should print True
-
-data = [235, 140, 4]  # Invalid UTF-8 encoding
-print(validUTF8(data))  # Should print False
+            byte_count -= 1
+    return byte_count == 0
