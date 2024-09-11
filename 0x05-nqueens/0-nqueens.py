66d1ccf4eabce6727d9b46a4d5f12c716f04fb28
root <izirenjoel@gmail.com>
2024-09-11 13:12:49 +0000

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,7 +5,6 @@ N Queens Problem: Solve the N Queens problem using backtracking.
 
 import sys
 
-
 def is_safe(board, row, col, n):
     """
     Check if it's safe to place a queen at board[row][col].
@@ -14,20 +13,16 @@ def is_safe(board, row, col, n):
     for i in range(col):
         if board[row][i] == 1:
             return False
-
     # Check the upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
     # Check the lower diagonal on the left side
     for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-
-return True
-
+    return True
 
 def solve_n_queens(board, col, n, solutions):
     """
@@ -38,21 +33,17 @@ def solve_n_queens(board, col, n, solutions):
         solution = [[i, row.index(1)] for i, row in enumerate(board)]
         solutions.append(solution)
         return True
-
     res = False
     for i in range(n):
         if is_safe(board, i, col, n):
             # Place the queen at the position board[i][col]
             board[i][col] = 1
-
             # Recur to place the rest of the queens
             solve_n_queens(board, col + 1, n, solutions)
-
             # If placing the queen here leads to no solution, backtrack
             board[i][col] = 0
 
     return res
-
 
 def n_queens(n):
     """
@@ -63,23 +54,19 @@ def n_queens(n):
     solve_n_queens(board, 0, n, solutions)
     return solutions
 
-
 if __name__ == "__main__":
     # Input validation
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     # Solve the N Queens problem and print the solutions
     solutions = n_queens(N)
     for solution in solutions:
